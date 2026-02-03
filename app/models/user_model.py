41da2e9088cdcf94b4d5b08a057d6cae2f5f4066
Kianwhile8 <kianwhiley6@gmail.com>
2026-02-03 01:34:08 +0000

    fixed indentation error

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -117,31 +117,31 @@ class UserModel:
             logger.error(f"Failed to update user: {e}")
             return None
         
-        def list_users(self, role= None, limit = 100):
+    def list_users(self, role= None, limit = 100):
 
-            "lists all userse, can be filtered by role"
-            if role:
-                query.add_filter("role", "=", role)
-                query.order=["-creaeed_at"] #newest first
+        "lists all userse, can be filtered by role"
+        if role:
+            query.add_filter("role", "=", role)
+            query.order=["-creaeed_at"] #newest first
 
-                results = list(query.fetch(limit=limit))
-                return [self._entity_to_diuct(entity) for entity in results]
+            results = list(query.fetch(limit=limit))
+            return [self._entity_to_diuct(entity) for entity in results]
 
-        def delete_user(self,email):
+    def delete_user(self,email):
 
-            """deletes user ( sets is_active to flase)"""
+        """deletes user ( sets is_active to flase)"""
 
-            return self.update_user(email, isactive=False) is not None
+        return self.update_user(email, isactive=False) is not None
 
-        def _entity_to_dict(self, entity):
-            """converts datastore entity to dicionary
-            internal methods so users dont have to directly call"""           
-            if not entity:
-                return None
-            # entity to dict
-            data = dict(entity)
-            #adds email to dict
-            data["email"] = entity.key.name
+    def _entity_to_dict(self, entity):
+        """converts datastore entity to dicionary
+        internal methods so users dont have to directly call"""           
+        if not entity:
+            return None
+        # entity to dict
+        data = dict(entity)
+        #adds email to dict
+        data["email"] = entity.key.name
 
-            return data
+        return data
         
